ad09a63b30862ec7e54fe5b65805da3148d1699b
Fehufanga <132999329+Fehufanga@users.noreply.github.com>
2023-09-01 15:34:58 +0700

    Fix issue with logging not working

--- dyk_logger.py
+++ dyk_logger.py
@@ -4,9 +4,10 @@ import pywikibot as pwb
 
 class DYKLogger():
     SITE = pwb.Site()
-    LOG_LOC = f"User:{SITE.username}/Log"
+    LOG_LOC = f"User:{SITE.username()}/Log"
 
     @classmethod
     def log(self, message):
         log = pwb.Page(self.SITE, self.LOG_LOC)
-        log.text = log.text + "*{now:%c} {message}\n".format(now=datetime.now(), message=message)
+        log.text = log.text + "*'''{now:%c}''': {message}\n".format(now=datetime.now(), message=message)
+        log.save("[BOT] Logging bot run")
